849e7edfbd9fb1dd7bbc63427b67c135b270f73b
ajay <ajayrajput9554@gmail.com>
2023-03-30 14:33:17 +0530

    Movie List App

---
+++ block-BRaabm/movie-list-app/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./component/Header";
+import Movies from "./component/Movies";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Movies />
+    </>
+  );
+}
+
+export default App;
